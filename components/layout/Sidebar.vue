4ec7a577df83a3106decb618cf4a8f1a98dcc92c
Alexnander Nasokin <alexnas2python@gmail.com>
2024-10-11 21:11:52 +0300

    Install NuxtIcon

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -6,7 +6,11 @@
       <NuxtImg src="/logo.svg" alt="" width="100px" class="mx-auto" />
     </NuxtLink>
 
-    <UiButton>Send</UiButton>
+    <!-- <UiButton>Send</UiButton> -->
+
+    <button class="absolute top-2 right-3 transition-colors hover:text-primary">
+      <Icon name="line-md:logout" size="20" />
+    </button>
   </aside>
 </template>
 
