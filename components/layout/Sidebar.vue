e8a21a507f8becc8d30ae18a3dcf7df5c020b7d2
Alexnander Nasokin <alexnas2python@gmail.com>
2024-10-11 15:03:04 +0300

    Install and prepare Shadcn UI

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -5,6 +5,8 @@
     <NuxtLink to="/" class="mb-10 block">
       <NuxtImg src="/logo.svg" alt="" width="100px" class="mx-auto" />
     </NuxtLink>
+
+    <UiButton>Send</UiButton>
   </aside>
 </template>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
-  modules: ['@nuxt/image', '@nuxtjs/tailwindcss'],
+  devtools: { enabled: false },
+  modules: ['@nuxt/image', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: 'Ui',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
+  },
 });
