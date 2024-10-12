37c5c9a2a0d1cdc6a6e9b37129da592eb08c90f8
Alexnander Nasokin <alexnas2python@gmail.com>
2024-10-12 17:45:32 +0300

    Make loader; make backend (AppWrite); make login page

---
+++ components/layout/Loader.vue
@@ -0,0 +1,13 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="wrapper">
+    <img src="/loader.svg" alt="" />
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  @apply bg-sidebar flex items-center justify-center w-screen h-screen;
+}
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,45 @@
+<script lang="ts" setup>
+useHead({
+  title: 'Login | CRM System',
+});
+
+const emailRef = ref();
+const passwordRef = ref();
+const nameRef = ref();
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen w-full">
+    <div class="rounded bg-sidebar w-1/4 p-5 min-w-[300px]">
+      <h1 class="font-bold text-2xl text-center mb-5">Login</h1>
+
+      <form>
+        <UiInput
+          placeholder="Email"
+          type="email"
+          class="mb-3"
+          v-model="emailRef"
+        />
+        <UiInput
+          placeholder="Password"
+          type="password"
+          class="mb-3"
+          v-model="passwordRef"
+        />
+        <UiInput
+          placeholder="Name"
+          type="name"
+          class="mb-3"
+          v-model="nameRef"
+        />
+
+        <div class="flex items-center justify-center gap-5">
+          <UiButton type="button">Login</UiButton>
+          <UiButton type="button">Register</UiButton>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
