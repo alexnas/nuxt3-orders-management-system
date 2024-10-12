f76938a331315d543686c54b12413c5f26e89455
Alexnander Nasokin <alexnas2python@gmail.com>
2024-10-12 18:49:14 +0300

    Install Pinia and create store

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,7 @@ export default defineNuxtConfig({
       },
     ],
     '@nuxt/icon',
+    '@pinia/nuxt',
   ],
   shadcn: {
     /**
@@ -29,6 +30,9 @@ export default defineNuxtConfig({
      * @default "./components/ui"
      */
     componentDir: './components/ui',
+  },
+  pinia: {
+    storesDirs: ['./store/**'],
   },
   runtimeConfig: {
     appWriteId: process.env.APP_WRITE_ID,
