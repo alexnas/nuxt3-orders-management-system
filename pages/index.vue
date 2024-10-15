148afe73104a5a07c0ccfc918a4c7c24d5dee87e
Alexnander Nasokin <alexnas2python@gmail.com>
2024-10-15 18:55:06 +0300

    Make kanban card drag-n-drop move funtionality

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,13 @@
 <script lang="ts" setup>
+import { useMutation } from '@tanstack/vue-query';
 import dayjs from 'dayjs';
 import type { ICard, IColumn } from '~/components/kanban/kanban.types';
 import { useKanbanQuery } from '~/components/kanban/useKanbanQuery';
 import { convertCurrency } from '~/lib/convertCurrency';
+import { DB } from '~/lib/appwrite';
+import type { EnumStatus } from '~/types/deals.types';
+const DB_ID = import.meta.env.VITE_DB_ID;
+const COLLECTION_DEALS = import.meta.env.VITE_COLLECTION_DEALS;
 
 useSeoMeta({
   title: 'Home | CRM System',
@@ -10,8 +15,38 @@ useSeoMeta({
 
 const dragCardRef = ref<ICard | null>(null);
 const sourceColumnRef = ref<IColumn | null>();
-
 const { data, isLoading, refetch } = useKanbanQuery();
+
+type TypeMutationVariables = {
+  docId: string;
+  status?: EnumStatus;
+};
+
+const { mutate } = useMutation({
+  mutationKey: ['move card'],
+  mutationFn: ({ docId, status }: TypeMutationVariables) =>
+    DB.updateDocument(DB_ID, COLLECTION_DEALS, docId, {
+      status,
+    }),
+  onSuccess: () => {
+    refetch();
+  },
+});
+
+const handleDragStart = (card: ICard, column: IColumn) => {
+  dragCardRef.value = card;
+  sourceColumnRef.value = column;
+};
+
+const handleDragOver = (event: DragEvent) => {
+  event.preventDefault();
+};
+
+const handleDrop = (targetColumn: IColumn) => {
+  if (dragCardRef.value && sourceColumnRef.value) {
+    mutate({ docId: dragCardRef.value.id, status: targetColumn.id });
+  }
+};
 </script>
 
 <template>
@@ -21,7 +56,13 @@ const { data, isLoading, refetch } = useKanbanQuery();
 
     <div v-else>
       <div class="grid grid-cols-5 gap-16">
-        <div v-for="(column, index) in data" :key="column.id" class="min-w-fit">
+        <div
+          v-for="(column, index) in data"
+          :key="column.id"
+          class="min-w-fit"
+          @dragover="handleDragOver"
+          @drop="() => handleDrop(column)"
+        >
           <div
             class="rounded bg-slate-700 py-1 px-5 mb-2 text-center overflow-x-auto"
           >
@@ -34,6 +75,7 @@ const { data, isLoading, refetch } = useKanbanQuery();
               :key="card.id"
               class="mb-5"
               draggable="true"
+              @dragstart="() => handleDragStart(card, column)"
             >
               <UiCardHeader role="button"
                 >{{ card.name }}
